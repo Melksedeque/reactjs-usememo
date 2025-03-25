5118b8bb89b2be6405ea29a7eedd1bc63f7d3b00
Melksedeque <freelancer@melksedeque.com.br>
2025-03-25 13:11:42 -0300

    Import useState e criação de input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [nomeDoImposto, setNomeDoImposto] = useState("");
 
   return (
-    <>
+    <div className="App">
       <input
         type="text"
         placeholder="Nome do Imposto"
         value={nomeDoImposto}
         onChange={(e) => setNomeDoImposto(e.target.value)}
       />
-    </>
+    </div>
   );
 }
 
